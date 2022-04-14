6d3b21fd4d176da4da7bfd7998555c1ed2fe8427
Benjamin Johnson <103455666+BenjaminJohnsonRev@users.noreply.github.com>
2022-04-14 15:49:22 -0400

    Add files via upload
    Cloned Rorie's exercises to jumpstart my repo.

---
+++ exercises/week1-exercise2/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	    Create a Rock-Paper-Scissors game that allows 2 people to play against each other.
+	    Requirements:
+	    - use Scanner to get user input
+	    - use control-flow statements to determine the winner and print it out
+	    - allow players to repeatedly play many games
+	    - must make use of at least one class
+	    - Make your own Git Repo and paste the link in the excel sheet.
+	    Stretch Goals:
+	    - allow players to enter their name for a more personalized experience
+	    - keep track of score
+	    - Extend your game to be tic-tac-toe instead of Rock-Paper-Scissors
+	 */
+    }
+}
